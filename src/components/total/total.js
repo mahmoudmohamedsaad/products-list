0af44af2488332c3ad41dd8d2cc19a38b5baa36e
Mahmoud Saad <62154958+mahmoudmohamedsaad@users.noreply.github.com>
2024-06-28 20:15:32 +0300

    Add files via upload

--- src/components/total/total.js
+++ src/components/total/total.js
@@ -4,8 +4,11 @@ const Total = (props) => {
     const {items} = props;
     let total = 0
     for (let i = 0; i < items.length; i++) {
-        total += parseFloat(items[i].price)
+        total = total + parseFloat(items[i].price * items[i].quantity)
     }
+    //anther solution
+    //const total = items.reduce((total, item) => total + item.quantity * item.price, 0);
+
     return (
         <div>
             <p className="text">Total Price: {total}</p>
