93f3e72d15f081f9c3e4f89937480ed036d76239
Mahmoud Saad <62154958+mahmoudmohamedsaad@users.noreply.github.com>
2024-06-28 20:14:42 +0300

    Add files via upload

--- src/components/addItem/addItem.js
+++ src/components/addItem/addItem.js
@@ -1,38 +1,76 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class addItem extends Component {
-    state = {
-        product: '',
-        price: ''
-    }
+class AddItem extends Component {
+  state = {
+    quantity: 1,
+    product: '',
+    price: 1,
+  };
 
-    handleChange = (e) => {
-        console.log(e.target.id + ": " + e.target.value)
-        this.setState({
-            [e.target.id]: e.target.value
-        })
-    }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        this.props.add(this.state)
-        this.setState({
-            product: '',
-            price: ''
-        })
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.add(this.state);
+    this.setState({
+      quantity: 1,
+      product: '',
+      price: 1,
+    });
+  };
 
-    render(){
-        return(
-            <div className="item">
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.state.product} placeholder="Enter Product" id="product" onChange={this.handleChange} required/>
-                    <input type="number" value={this.state.price} placeholder="Enter Price" id="price" onChange={this.handleChange} required/>
-                    <input type="submit" value="Add"/>
-                </form>
-            </div>
-        )
-    }
+  handleIncrement = (name) => {
+    this.setState((prevState) => ({
+      [name]: parseInt(prevState[name]) + 1,
+    }));
+  };
+
+  handleDecrement = (name) => {
+    this.setState((prevState) => ({
+      [name]: Math.max(1, parseInt(prevState[name]) - 1),
+    }));
+  };
+
+  render() {
+    return (
+      <div className="item">
+        <form onSubmit={this.handleSubmit}>
+          <div className='adding'>
+            <button type="button" onClick={() => this.handleDecrement('quantity')}>-</button>
+            <input
+              type="number"
+              name="quantity"
+              value={this.state.quantity}
+              readOnly
+            />
+            <button type="button" onClick={() => this.handleIncrement('quantity')}>+</button>
+          </div>
+          <input
+            type="text"
+            value={this.state.product}
+            placeholder="Enter Product"
+            id="product"
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="number"
+            value={this.state.price}
+            placeholder="Price per piece"
+            id="price"
+            min="1"
+            onChange={this.handleChange}
+            required
+          />
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default addItem;
+export default AddItem;
